21b802ce9ce5ffdd0ad8f1dd8d8b783e346dcee4
Matt Brown <gitmattb@gmail.com>
2022-06-08 23:02:38 -0700

    Update settingsloader to load datetime

--- settingsloader.py
+++ settingsloader.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 # Loads settings from a filepath in setting:value format
 # if must_match is true, then a settings_dict must be passed. Values will only be UPDATED
 # if must_have_value is true then if
@@ -30,29 +32,42 @@ def __check_dict_int(setdic, key, default) -> None:
     else:
         setdic[key] = int(setdic[key])
 
+def __check_dict_bool(setdic, key, default = False) -> None:
+    if key not in setdic:
+        setdic[key] = default
+    else:
+        setdic[key] = setdic[key].lower() == 'true'
+def __check_dict_datetime(setdic, key, default) -> None:
+    if key not in setdic:
+        setdic[key] = datetime.strptime(default, '%H:%M').time()
+    else:
+        setdic[key] = datetime.strptime(setdic[key],'%H:%M').time();
+
 # loads user settings, does some checks and casts
 def load_user_settings(filepath, settings_dict = None) -> dict:
     settings_dict = load_settings(filepath, settings_dict)
 
     mandatory_user_settings = ['email', 'password']
 
-    count = 0
+    errorcount = 0
     for setting in mandatory_user_settings:
         if setting not in settings_dict:
             print("ERROR: {} setting not set!".format(setting))
-            count += 1
+            errorcount += 1
     
-    if count > 0:
+    if errorcount > 0:
         quit()
 
-    if 'auto_solve_captchas' not in settings_dict:
-        settings_dict['auto_solve_captchas'] = False
-    else:
-        settings_dict['auto_solve_captchas'] = settings_dict['auto_solve_captchas'].lower() == 'true'
+    default_bools = { 'auto_solve_captchas':False, 'enable_nightmode': False }
+    for key, val in default_bools.items():
+        __check_dict_bool(settings_dict, key, val)
 
     default_ints = {'notify_soldier_amt':60, 'min_checktime_secs':300, 'max_checktime_secs':600 }
-
     for key,val in default_ints.items():
         __check_dict_int(settings_dict, key, val)
+
+    default_datetime = {'nightmode_begin': '00:00', 'nightmode_end': '9:00'}
+    for key,val in default_datetime.items():
+        __check_dict_datetime(settings_dict, key, val)
 
     return settings_dict
