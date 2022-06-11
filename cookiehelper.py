589e8b960c8ad8a72c7819c665c0d4976eaf7274
Matt Brown <gitmattb@gmail.com>
2022-06-11 00:40:06 -0700

    Add ability to load cookies from browsers

--- cookiehelper.py
+++ cookiehelper.py
@@ -1,9 +1,31 @@
 import pickle
+import browser_cookie3
+from urllib.parse import urlparse
+ # py -m pip install browser-cookie3
 
-def save_cookies(requests_cookiejar, filename):
-    with open(filename, 'wb') as f:
+def save_cookies_to_path(requests_cookiejar, filepath):
+    with open(filepath, 'wb') as f:
         pickle.dump(requests_cookiejar, f)
 
-def load_cookies(filename):
+def load_cookies_from_path(filename):
     with open(filename, 'rb') as f:
         return pickle.load(f)
+
+
+def load_cookies_from_browser(browser: str, websiteurl: str = None):
+    browser = browser.lower()
+    if websiteurl is not None:
+        dom = urlparse(websiteurl).netloc
+        dom = '.'.join(dom.split('.')[-2:])
+    else:
+        dom = None
+    if browser == 'firefox':
+        cj = browser_cookie3.firefox(domain_name=dom)
+    elif browser == 'chrome':
+        cj= browser_cookie3.chrome()
+    else:
+        return None
+
+    return cj
+
+
