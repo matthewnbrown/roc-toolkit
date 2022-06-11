6e39f5333e1b4b58e9ea52f2ffa385b778b0b91a
Matt Brown <gitmattb@gmail.com>
2022-06-11 01:24:01 -0700

    Add initial testing file

---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class UserSettingsTest(unittest.TestCase):
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+
+if __name__ == "__main__":
+    unittest.main()
