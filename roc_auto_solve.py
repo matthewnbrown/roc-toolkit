48a84d68f4f363fc3e59266299dd794c40182ab9
Matt Brown <gitmattb@gmail.com>
2022-06-10 20:08:46 -0700

    Remove unused variable in auto_solver

--- roc_auto_solve.py
+++ roc_auto_solve.py
@@ -4,7 +4,6 @@ def Solve(api_key, imgpath) -> str:
     solver = TwoCaptcha(api_key)
     try:
         result = solver.normal(imgpath, numeric = 1, min_len = 1, max_len = 1)['code']
-        success = True
     except api.ApiException as e:
         result = e.args[0]
     except NetworkException as e:
