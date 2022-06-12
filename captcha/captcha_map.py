134a211d70aff1990c244d4a8fa1f64a4ddd7b97
Matt Brown <gitmattb@gmail.com>
2022-06-12 02:26:31 -0700

    Add base captcha map

---
+++ captcha/captcha_map.py
@@ -0,0 +1,31 @@
+from genericpath import exists
+import os
+from roc_settings.settingstools import SettingsSaver, SettingsLoader
+
+class CaptcahMap:
+    def __init__(self, filename = 'captcha/map.txt') -> None:
+        self.filename = filename
+        self.map = {}
+        self.__load_map()
+    
+    def __contains__(self, key):
+        return key in self.map
+
+    def __load_map(self):
+        if not os.path.exists(self.filename):
+            SettingsSaver.save_settings_toPath(self.filename, self.map)
+            return
+        SettingsLoader.load_settings_from_path(self.filename, self.map, False)
+    
+    def __save_map(self):
+        SettingsSaver.save_settings_toPath(self.filename, self.map)
+
+    def __append_captcha(self, hash: str, val: str):
+        pass
+    def add_captcha(self, hash: str, val:str, append_to_file: True):
+        self.map[hash] = val
+        
+        if append_to_file:
+            self.__append_captcha(hash, val)
+
+
