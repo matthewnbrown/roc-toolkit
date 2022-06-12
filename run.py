3958e734bc94200974a17ccf74717a25eac7c25d
Matt Brown <gitmattb@gmail.com>
2022-06-12 02:25:04 -0700

    Add message when exiting due to settings

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ if __name__== '__main__':
     site_settings_fp = 'site.settings'
 
     if SettingsFileMaker.needs_user_setup(user_settings_fp, site_settings_fp):
+        print("Exiting. Please fill out settings files")
         quit()
 
     us = UserSettings(filepath=user_settings_fp)
