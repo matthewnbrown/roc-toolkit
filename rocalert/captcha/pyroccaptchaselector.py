f586c8e545b6a37e668305dfe2204c44962b5967
Matt Brown <gitmattb@gmail.com>
2022-06-27 02:18:35 -0700

    Update captchaselector to handle different pages

--- rocalert/captcha/pyroccaptchaselector.py
+++ rocalert/captcha/pyroccaptchaselector.py
@@ -1,7 +1,36 @@
 from numpy import random # py -m pip install numpy
 
 class ROCCaptchaSelector():
+    __btn_dimensions = (40, 30)
+    __keypadTopLeft = { 'roc_recruit': [890, 705], 'roc_armory': [973,1011]}
+    __keypadGap = [52, 42]
+
+    def __init__ (self, resolution = None) -> None:
+        self.resolution = resolution
+
+    def get_xy(self, number):
+        pass
+
+    def get_xy_static(self, number, page):
+        if page not in self.__keypadTopLeft:
+            raise Exception(f'Page {page} does not have coordinates for captchas!')
+        number = int(number) - 1
+        x_btn = self.__keypadTopLeft[page][0] + (number % 3) * self.__keypadGap[0]
+        y_btn = self.__keypadTopLeft[page][1] + (number // 3) * self.__keypadGap[1]
+
+        x_click = -x_btn
+        while x_click < x_btn or x_click > x_btn + self.__btn_dimensions[0]:
+            x_click = x_btn + random.normal(0, self.__btn_dimensions[0]/3)
+        y_click = -y_btn
+        while y_click < y_btn or y_click > y_btn + self.__btn_dimensions[1]:
+            y_click = y_btn + random.normal(0, self.__btn_dimensions[1]/3)
+
+        return (int(x_click), int(y_click))
+
+
+class ROCCaptchaSelector_deprecated():
     __btn_dimensions = (45, 35)
+
     __btn_locations = { 
         '1': (898, 732), '2': (950, 732), '3': (1003, 732), 
         '4': (898, 772), '5': (950, 772), '6': (1003, 772), 
