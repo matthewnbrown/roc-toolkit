26acfa95e6e3f582146aa6aa2cc1281f0983278c
Matt Brown <gitmattb@gmail.com>
2022-07-01 13:49:15 -0700

    Clean up formatting

--- rocalert/remote_lookup.py
+++ rocalert/remote_lookup.py
@@ -1,6 +1,7 @@
 import requests
 from rocalert.roc_web_handler import Captcha
 
+
 class RemoteCaptcha():
     def __init__(self, add_url, lookup_url) -> None:
         self.__add_url = add_url
@@ -9,23 +10,20 @@ class RemoteCaptcha():
     def __get_result(self, url, payload):
         try:
             resp = requests.get(url, payload).text
-        except Exception as e:
+        except Exception:
             resp = 'ERROR ACCESSING REMOTE'
         return resp
 
     def lookup_remote(self, captcha: Captcha) -> str:
         if self.__lookup_url is None:
             return None
-        
-        payload = { 'hash': captcha.hash }
+
+        payload = {'hash': captcha.hash}
         return self.__get_result(self.__lookup_url, payload)
 
     def add_remote(self, captcha: Captcha) -> str:
         if self.__add_url is None:
             return None
 
-        payload = { 'hash':captcha.hash, 'answer':captcha.ans }
+        payload = {'hash': captcha.hash, 'answer': captcha.ans}
         return self.__get_result(self.__add_url, payload)
-
-
-
