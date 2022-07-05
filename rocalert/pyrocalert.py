7747ae3667c760d746b2f50246f73b6785ecd40f
Matt Brown <gitmattb@gmail.com>
2022-07-05 15:46:24 -0700

    Catch timeout exception

--- rocalert/pyrocalert.py
+++ rocalert/pyrocalert.py
@@ -172,7 +172,7 @@ class RocAlert:
         if len(ans) != 1 or ans not in self.validans:
             self.__log(("Warning: received response \'{}\' "
                         + "from captcha solver!").format(ans))
-            if 'bad response: 503' in ans or 'Exception' in ans:
+            if 'bad response: 503' in ans or 'exception' in ans.lower():
                 waittime = 5 * (1 + self.consecutive_answer_errors**2)
                 self.__log(
                     f"Waiting {waittime} seconds before attempting again..."
