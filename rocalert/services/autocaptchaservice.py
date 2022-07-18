e1b8268772c4294fd6e69b328f607a72a1e7aa50
Matt Brown <gitmattb@gmail.com>
2022-07-17 18:37:51 -0700

    Add twocaptcha solving to autocaptchaservice

---
+++ rocalert/services/autocaptchaservice.py
@@ -0,0 +1,51 @@
+from twocaptcha import TwoCaptcha, TimeoutException
+from twocaptcha.api import ApiException, NetworkException
+from rocalert.roc_settings.settingstools import UserSettings
+from rocalert.roc_web_handler import RocWebHandler
+from rocalert.roc_web_handler import Captcha
+from rocalert.services.rocservice import RocService
+
+
+class AutoCaptchaService(RocService):
+    """Service that uses 2Captcha for captcha solving"""
+    def __is_twocpatch_key_invalid(self, twocaptcha_key):
+        return twocaptcha_key is None or len(twocaptcha_key) == 0
+
+    def __create_solver(self, key: str) -> TwoCaptcha:
+        solver = TwoCaptcha(key)
+        return solver
+
+    def __twocaptcha_solve(self, api_key, img_path) -> str:
+        ans = None
+        response = None
+        if self.__is_twocaptcha_key_invalid():
+            return {
+                'ans': ans, 
+                'error': 'ERROR_TWOCAPTCHA_KEY_NOT_SET',
+                'response': response}
+
+        try:
+            solver = self.__create_solver(key=api_key)
+            hinttext = 'Single digit between 1-9  (1, 2, 3, 4, 5, 6, 7, 8, 9)'
+            response = solver.normal(img_path, hintText=hinttext)
+            ans = response['code']
+            error = None
+        except ApiException as exception:
+            error = f'Exception: {exception.args[0]}'
+            if 'ZERO_BALANCE' in exception.args[0]:
+                error = f'Critical Exception: {exception.args[0]}'
+        except NetworkException as exception:
+            error = f'Exception: {exception.args[0]}'
+        except TimeoutException as exception:
+            error = f'Exception: {exception.args[0]}'
+
+        return {'ans': ans, 'response': response, 'error': error}
+
+    """Runs the service"""
+    def run_service(
+            self,
+            roc: RocWebHandler = None,
+            settings: UserSettings = None,
+            custom_settings: dict = None
+            ) -> bool:
+        pass
