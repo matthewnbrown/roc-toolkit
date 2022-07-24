83323489a4502efc56fdf25bb1462d0ad357c777
Matt Brown <gitmattb@gmail.com>
2022-07-23 19:47:36 -0700

    Basic login service implementation

--- rocalert/services/loginservice.py
+++ rocalert/services/loginservice.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from rocalert.roc_web_handler import RocWebHandler
 from rocalert.services.rocservice import RocService
 from rocalert.roc_settings.settingstools import UserSettings
@@ -28,4 +29,31 @@ class LoginService(RocService):
                 'result': 'success' or 'failure'
                 'error': None if success, else general explanation of failure
         """
-        pass
+        r, e = 'result', 'error'
+        if roc is None:
+            return {r: 'failure', e: 'roc session not passed in request'}
+        if settings is None:
+            return {r: 'failure', e: 'User settings not passed in request'}
+        if custom_settings is None:
+            return {r: 'failure', e: 'No custom settings passed in request'}
+        if 'site_settings' not in custom_settings:
+            return {r: 'failure', e: '\'site_settings\' not passed in request'}
+        if custom_settings['site_settings'] is None:
+            return {r: 'failure', e: '\'site_settings\' is none'}
+        if roc.is_logged_in():
+            return {r: 'failure', e: 'ROC session already logged in'}
+
+        repeated_failures = 0
+
+        while repeated_failures < 3:
+            res = roc.login(
+                settings.get_setting('email'),
+                settings.get_setting('password'))
+
+            if 'success' in res:
+                return {r: 'success', e: None}
+            if 'incorrect_login' in res:
+                return {r: 'failure', e: 'Incorrect login'}
+            if 'general_failure' in res:
+                repeated_failures += 1
+        return {r: 'failure', e: 'Critical error: Too many failure attempts'}
