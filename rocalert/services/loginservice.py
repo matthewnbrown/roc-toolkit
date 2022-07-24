d68ec6e90aeb0db5359cea0662ca06f23d1ce1c3
Matt Brown <gitmattb@gmail.com>
2022-07-23 17:10:10 -0700

    Add base login service file

---
+++ rocalert/services/loginservice.py
@@ -0,0 +1,29 @@
+from rocalert.roc_web_handler import RocWebHandler
+from rocalert.services.rocservice import RocService
+from rocalert.roc_settings.settingstools import UserSettings
+
+
+class LoginService(RocService):
+    """_summary_
+    Attempts to login to ROC
+    """
+    def run_service(
+            self,
+            roc: RocWebHandler = None,
+            settings: UserSettings = None,
+            custom_settings: dict = None
+            ) -> bool:
+        """_summary_
+
+        Args:
+            roc (RocWebHandler, optional): ROC session used to login.
+                Defaults to None.
+            settings (UserSettings, optional): Settings file to use,
+                must have username and password. Defaults to None.
+            custom_settings (dict, optional): Must contain 'site_settings'
+                Defaults to None.
+
+        Returns:
+            bool: Returns dictionary with 'result' or 'error item
+        """
+        pass
