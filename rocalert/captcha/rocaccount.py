1a75ea619840c68dedd9c67ddc94ed91abce1c88
Matt Brown <gitmattb@gmail.com>
2022-08-18 18:50:45 -0700

    Add bare account classes

---
+++ rocalert/captcha/rocaccount.py
@@ -0,0 +1,13 @@
+class RocAccount:
+    def __init__(self) -> None:
+        pass
+
+
+class RocSoldiers:
+    def __init__(self) -> None:
+        pass
+
+
+class RocArmoury:
+    def __init__(self) -> None:
+        pass
