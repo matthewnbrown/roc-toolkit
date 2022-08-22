d06f3d028b473c79bffd4334bd4e844fc4fd7bcb
Matt Brown <gitmattb@gmail.com>
2022-08-22 00:56:33 -0700

    Remove unneeded import

--- rocalert/services/loginservice.py
+++ rocalert/services/loginservice.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from rocalert.roc_web_handler import RocWebHandler
 from rocalert.services.rocservice import RocService
 from rocalert.roc_settings.settingstools import UserSettings
