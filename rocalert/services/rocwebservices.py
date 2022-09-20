b3b668d0010330931154e4c617bb7f38c288b764
Matt Brown <gitmattb@gmail.com>
2022-09-20 11:47:02 -0700

    Implement run_service skeleton

--- rocalert/services/rocwebservices.py
+++ rocalert/services/rocwebservices.py
@@ -1,12 +1,13 @@
-from typing import List
+from typing import Dict, List
 from bs4 import BeautifulSoup
 from rocalert.roc_web_handler import RocWebHandler
+from rocalert.rocaccount import BattlefieldTarget
 
 
 class BattlefieldPageService():
 
     @classmethod
-    def run_service(cls, roc: RocWebHandler, pagenum: int) -> List:
+    def run_service(cls, roc: RocWebHandler, pagenum: int) -> Dict:
         pageurl = roc.site_settings['roc_home'] + \
             f'/battlefield.php?p={pagenum}'
 
@@ -20,6 +21,15 @@ class BattlefieldPageService():
                 'response': 'error',
                 'error': f'Invalid page number {pagenum}'}
 
+        players = content.find('ul')
+
+        targets = cls._get_alltargets(players)
+
+        return {
+            'response': 'success',
+            'result': targets
+        }
+
     @classmethod
     def _checkvalidpage(cls, content: BeautifulSoup, pagenum: int) -> bool:
         pagerangetext = content.contents[1].contents[1].text
@@ -29,3 +39,13 @@ class BattlefieldPageService():
             1 + usercount % 50
 
         return 1 <= pagenum <= pagecount
+
+    @classmethod
+    def _get_alltargets(cls,
+                        players: BeautifulSoup
+                        ) -> List[BattlefieldTarget]:
+        pass
+
+    @classmethod
+    def _get_bftarget(cls, usercontent: BeautifulSoup) -> BattlefieldTarget:
+        pass
