029c11c6018e0230462a0bcce92ea15bbedf3433
Matt Brown <gitmattb@gmail.com>
2022-09-24 14:36:52 -0700

    Implement GetCaptchaService

--- rocalert/services/captchaservices.py
+++ rocalert/services/captchaservices.py
@@ -1,10 +1,11 @@
 from requests import Response
+from rocalert.roc_web_handler import RocWebHandler
 from rocalert.roc_web_handler import Captcha
 
 
 class GetCaptchaService:
     @classmethod
-    def run_service(cls, response: Response) -> Captcha:
+    def run_service(cls, response: Response, roc: RocWebHandler) -> Captcha:
         """_summary_
 
         Args:
@@ -16,10 +17,51 @@ class GetCaptchaService:
                 Captcha pulled from page.
                 Will be None if no captcha is detected
         """
+
+        if roc is None:
+            raise Exception('ROC parameter must exist')
+        elif response is None:
+            raise Exception('Response parameter must exist')
+
         captcha_type = cls._detectcaptchatype(response)
 
-        if captcha_type is None:
+        if captcha_type == Captcha.CaptchaType.IMAGE:
+            return cls._get_img_captcha(response)
+        if captcha_type == Captcha.CaptchaType.EQUATION:
+            return cls._get_equation_captcha(response)
+        if captcha_type == Captcha.CaptchaType.TEXT:
+            return cls._get_text_captcha(response)
+
+        return None
+
+    @classmethod
+    def _get_img_hash(cls, pagetext: str) -> str:
+        index = pagetext.find('img.php?hash=')
+        if index == -1:
             return None
+        endIndex = pagetext.find('"', index, index+100)
+        imghash = pagetext.text[index + len('img.php?hash='): endIndex]
+        return imghash
+
+    @classmethod
+    def _get_img_captcha(cls, resp: Response, roc: RocWebHandler) -> Captcha:
+        imghash = cls._get_img_hash(resp.text)
+        imgbytes = roc.get_imgcap_from_hash(imghash)
+        return Captcha(imghash, imgbytes, captype=Captcha.CaptchaType.IMAGE)
+
+    @classmethod
+    def _get_equation_captcha(cls, resp: Response) -> Captcha:
+        index = resp.text.find('<h1>What is')
+        if index == -1:
+            return None
+        endIndex = resp.text.find('</h1>', index, index+100)
+        equation = resp.text[index + len('<h1>What is'):  endIndex]
+        equation = equation.strip()[:-1]
+        return Captcha(equation, captype=Captcha.CaptchaType.EQUATION)
+
+    @classmethod
+    def _get_text_captcha(cls, resp: Response, roc: RocWebHandler) -> Captcha:
+        return Captcha('TEXT', captype=Captcha.CaptchaType.TEXT)
 
     @classmethod
     def _has_text_cap(cls, response: Response) -> bool:
