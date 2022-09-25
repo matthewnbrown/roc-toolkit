b8543463c512754234c3dacaf6436cb8dd4b84d6
Matt Brown <gitmattb@gmail.com>
2022-09-24 23:47:31 -0700

    Make RocCaptcha a normal class Add is_solved and answer properties

--- rocalert/captcha/captcha.py
+++ rocalert/captcha/captcha.py
@@ -1,13 +1,27 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod, abstractproperty
 
 
-class RocCaptcha(ABC):
+class RocCaptcha:
     def __init__(self) -> None:
         super().__init__()
+        self._answer = None
+        self._payload = None
+
+    @property
+    def is_solved(self) -> bool:
+        return self._answer is not None
+
+    @abstractproperty
+    def answer(self):
+        raise NotImplementedError
+
+    @answer.setter
+    def answer(self, new_answer) -> None:
+        raise NotImplementedError
 
     @abstractmethod
     def get_payload(self):
-        raise NotImplementedError('get_payload is not implemented')
+        raise NotImplementedError
 
 
 class ImageCaptcha(RocCaptcha):
