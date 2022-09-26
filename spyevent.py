90db134132cc95ce219489ed896c1d625cdb7f31
Matt Brown <gitmattb@gmail.com>
2022-09-26 00:00:25 -0700

    Move multicaptchagui to CaptchaServices

--- spyevent.py
+++ spyevent.py
@@ -3,12 +3,9 @@ from random import random
 from time import time
 from typing import Callable, Deque, Iterable, List, Set
 from os.path import exists
-from functools import partial
 from threading import Thread, Lock
-from tkinter import Entry, Canvas, Button, Frame, PhotoImage, Tk, NW, Event
-from PIL import Image, ImageTk
-import cv2
-import numpy as np
+
+
 from rocalert.captcha.equation_solver import EquationSolver
 
 from rocalert.roc_settings.settingstools import SettingsFileMaker, \
@@ -17,6 +14,7 @@ from rocalert.roc_web_handler import Captcha, RocWebHandler
 from rocalert.rocaccount import BattlefieldTarget
 from rocalert.cookiehelper import load_cookies_from_path, \
     load_cookies_from_browser, save_cookies_to_path
+from rocalert.services.captchaservices import MulticaptchaGUI
 from rocalert.services.rocwebservices import BattlefieldPageService
 
 # Comma separated ids. Put your own ID in here
@@ -335,182 +333,6 @@ class SpyEvent:
         self._running = True
 
         self._gui.start_event()
-
-
-def _bytesimage_to_photoimage_resize(
-        image,
-        newx: int = 150,
-        newy: int = 150
-        ) -> PhotoImage:
-    nparr = np.frombuffer(image, dtype=np.uint8)
-    img_np = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
-    img_np_resize = cv2.resize(
-                    img_np, (newx, newy), fx=0, fy=0,
-                    interpolation=cv2.INTER_CUBIC)
-    photoimage = ImageTk.PhotoImage(image=Image.fromarray(img_np_resize))
-    return photoimage
-
-
-class MulticaptchaGUI:
-    def __init__(
-            self,
-            captchas: List[Captcha],
-            on_selection: Callable,
-            getnewcaptchas: Callable,
-            captcha_xcount: int = 4,
-            captcha_ycount: int = 1
-            ) -> None:
-        """_summary_
-
-        Args:
-            captchas (List[Captcha]): _description_
-                List of inital set of captchas
-            on_selection (Callable): _description_
-                Callback function that will be passed the solved captchas
-            getnewcaptchas (Callable): _description_
-                Callback function that should return a list of captchas
-            captcha_xcount (int, optional): _description_. Defaults to 4.
-            captcha_ycount (int, optional): _description_. Defaults to 1.
-        """
-        self._root = Tk()
-        self._root.call('wm', 'attributes', '.', '-topmost', '1')
-
-        self._update_view_lock = Lock()
-        self._modifycaptchas_lock = Lock()
-
-        self._captchas = deque(captchas)
-        images = self._create_imgs_from_captchas(captchas)
-        self._images = deque(images)
-
-        self._xcaptchacount = captcha_xcount
-        self._ycaptchacount = captcha_ycount
-
-        self._onselect = on_selection
-        self._getcaptchas = getnewcaptchas
-
-        self.entry = Entry(self._root)
-        self.user_num_selection = '0'
-
-        self._root.bind("<Key>", self.__on_keypress)
-
-        self._canvasframe = Frame(self._root)
-        self._canvases = [Canvas(self._canvasframe,
-                                 height=150,
-                                 width=150)
-                          for i in range(self._captchawindowscount)]
-
-        for y in range(captcha_ycount):
-            for x in range(captcha_xcount):
-                self._canvases[x + y * captcha_xcount].grid(row=y, column=x)
-
-        self._canvasframe.grid(row=0, column=0)
-
-        frame = Frame(self._root)
-        frame.grid(row=captcha_ycount, column=0)
-
-        self._add_buttons(frame)
-        self._update_captcha_view()
-
-    @classmethod
-    def _create_imgs_from_captchas(cls,
-                                   captchas: Iterable[Captcha]
-                                   ) -> List[Captcha]:
-        res = []
-        for captcha in captchas:
-            res.append(_bytesimage_to_photoimage_resize(captcha.img))
-
-        return res
-
-    @property
-    def _captchawindowscount(self) -> int:
-        return self._xcaptchacount * self._ycaptchacount
-
-    def _update_captcha_view(self) -> None:
-        self._update_view_lock.acquire()
-
-        for i in range(min(len(self._captchas), self._captchawindowscount)):
-            self._canvases[i].delete('all')
-            self._canvases[i].create_image(0, 0,
-                                           anchor=NW,
-                                           image=self._images[i])
-
-        for i in range(self._captchawindowscount - len(self._captchas)):
-            self._canvases[self._captchawindowscount - i-1].delete('all')
-
-        self._update_view_lock.release()
-
-    def _get_new_captchas(self) -> None:
-        diff = self._captchawindowscount*2 - len(self._captchas)
-        if diff > 0:
-            self._getcaptchas(
-                self._captchawindowscount*2 - len(self._captchas))
-
-    def __on_keypress(self, event: Event):
-        key = event.keysym
-        if key.isnumeric() and int(key) > 0:
-            self._answer_selected(key)
-
-    def __on_button_click(self, num_str):
-        self._answer_selected(num_str)
-
-    def _add_buttons(self, root):
-        rows = 3
-        cols = 3
-        for i in range(rows):
-            for j in range(cols):
-                num = str(i*(rows) + j+1)
-                action_with_arg = partial(self.__on_button_click, num)
-                but = Button(root, text=num, command=action_with_arg)
-                but.grid(row=i, column=j)
-
-    def _answer_selected(self, answer: str) -> None:
-        if len(self._images) <= 0:
-            return
-
-        self._modifycaptchas_lock.acquire()
-        self._images.popleft()
-        cap = self._captchas.popleft()
-        self._modifycaptchas_lock.release()
-
-        cap.ans = answer
-        self._onselect(cap)
-        self._get_new_captchas()
-        self._update_captcha_view()
-
-    def _remove_captcha(self, captcha: Captcha) -> None:
-        index = self._captchas.index(captcha)
-        del self._captchas[index]
-        del self._images[index]
-
-    def add_captchas(self, newcaptchas: List[Captcha]) -> None:
-        self._modifycaptchas_lock.acquire()
-        print(f'Adding {len(newcaptchas)} captchas')
-        no_caps = len(self._images) == 0
-
-        newimgs = self._create_imgs_from_captchas(newcaptchas)
-        self._captchas.extend(newcaptchas)
-        self._images.extend(newimgs)
-
-        if no_caps:
-            self._update_captcha_view()
-
-        self._modifycaptchas_lock.release()
-
-    def remove_captchas(self, captchas: Iterable) -> None:
-        self._modifycaptchas_lock.acquire()
-        for captcha in captchas:
-            self._remove_captcha(captcha)
-
-        self._update_captcha_view()
-        self._modifycaptchas_lock.release()
-
-        self._get_new_captchas()
-
-    def start_event(self) -> None:
-        self._root.mainloop()
-
-    def end_event(self) -> None:
-        self._root.destroy()
 
 
 def test_multiimage(
