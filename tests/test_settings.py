b0dc6c9b3ed9dea43da9ea9089affb164dace759
Matt Brown <gitmattb@gmail.com>
2022-09-30 16:20:44 -0700

    Update settings tests to work with new settings implementation

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,17 +1,24 @@
 import os
 import unittest
 from datetime import datetime
-from rocalert.roc_settings.settingstools import SettingsSaver
+from rocalert.roc_settings.settingstools import SettingsSaver,\
+    SettingsLoader, Setting
+
 
 class SettingsSaverTest(unittest.TestCase):
     def __init__(self, methodName: str = ...) -> None:
         super().__init__(methodName)
         self.filename = 'SettingsSaverTest39920fe4902e79e63f4078fc2e61e481'
 
+    def __del__(self):
+        if os.path.exists(self.filename):
+            print('Warning: File cleanup required')
+            os.remove(self.filename)
+
     def load_settings(self) -> dict:
         with open(self.filename) as f:
             lines = f.readlines()
-        
+
         settings = {}
         for line in lines:
             setting, val = line.strip().split(':', maxsplit=1)
@@ -19,9 +26,9 @@ class SettingsSaverTest(unittest.TestCase):
 
         return settings
 
-    def SettingSaveLoad(self, settings) -> dict:
+    def settings_saveload(self, settings) -> dict:
         SettingsSaver.save_settings_toPath(self.filename, settings)
-        res = self.load_settings()
+        res = SettingsLoader.load_settings_from_path(self.filename, {})
         os.remove(self.filename)
         return res
 
@@ -30,69 +37,95 @@ class SettingsSaverTest(unittest.TestCase):
         with open(self.filename, 'w') as f:
             f.write(text)
             f.write('\n')
-        
-        settings = {'a':1, 'b':2, 'c':3}
+
+        settings = {
+            'a': Setting('aset', 'asetn', 15, int),
+            'b': Setting('bset', 'bsetn', 'bsetting', str),
+            'c': Setting('cset', 'csetn', 15.33, float)
+            }
 
         SettingsSaver.save_settings_toPath(self.filename, settings)
-        
+
         with open(self.filename, 'r') as f:
             input_text = f.read()
-        
+
         os.remove(self.filename)
 
         self.assertFalse(text in input_text)
 
     def test_int_saving(self):
-        settings = { 'test': 5 }
-        res = self.SettingSaveLoad(settings)
-        self.assertEqual(res['test'], '5')
-    
+        settings = {'test': Setting('TestSet', 'test', 5, int)}
+        res = self.settings_saveload(settings)
+        self.assertEqual(int(res['test'].value), 5)
+
     def test_true_bool_saving(self):
-        settings = { 'test': True }
-        res = self.SettingSaveLoad(settings)
-        self.assertEqual(res['test'], 'True')
+        settings = {'test': Setting('TestSet', 'test', True, bool)}
+        res = self.settings_saveload(settings)
+        self.assertEqual(res['test'].value, 'True')
 
     def test_false_bool_saving(self):
-        settings = { 'test': False }
-        res = self.SettingSaveLoad(settings)
-        self.assertEqual(res['test'], 'False')
+        settings = {'test': Setting('TestSet', 'test', False, bool)}
+        res = self.settings_saveload(settings)
+        self.assertEqual(res['test'].value, 'False')
 
     def test_string_saving(self):
-        settings = { 'test': 'abc123' }
-        res = self.SettingSaveLoad(settings)
-        self.assertEqual(res['test'], 'abc123')
+        settings = {'test': Setting('TestSet', 'test', 'abc123', str)}
+        res = self.settings_saveload(settings)
+        self.assertEqual(res['test'].value, 'abc123')
 
     def test_time_saving_short(self):
-        settings = { 'test': datetime.strptime('12:34', '%H:%M').time() }
-        res = self.SettingSaveLoad(settings)
-        self.assertEqual(res['test'], '12:34:00')
+        settings = {'test': Setting
+                    ('TestSet', 'test',
+                     datetime.strptime('12:34', '%H:%M').time(), datetime)}
+        res = self.settings_saveload(settings)
+        self.assertEqual(res['test'].value, '12:34:00')
 
     def test_time_saving_long(self):
-        settings = { 'test': datetime.strptime('12:34:56', '%H:%M:%S').time() }
-        res = self.SettingSaveLoad(settings)
-        self.assertEqual(res['test'], '12:34:56')
+        settings = {'test': Setting
+                    ('TestSet', 'test',
+                     datetime.strptime('12:34:56', '%H:%M:%S').time(),
+                     datetime)}
+        res = self.settings_saveload(settings)
+        self.assertEqual(res['test'].value, '12:34:56')
 
     def test_proper_number_saved(self):
-        settings = { 't1': 1, 't2': True, 't3': 'abc123'}
-        res = self.SettingSaveLoad(settings)
+        settings = {'t1': Setting('TestSet', 't1', 1, int),
+                    't2': Setting('TestSet', 't2', True, bool),
+                    't3': Setting('TestSet', 't3', 'abc123', str)
+                    }
+        res = self.settings_saveload(settings)
         self.assertEqual(len(settings), len(res))
 
     def test_settings_key_match(self):
-        settings = { 't1': 1, 't2': True, 't3': 'abc123'}
-        res = self.SettingSaveLoad(settings)
-        
+        settings = {'t1': Setting('TestSet', 't1', 1, int),
+                    't2': Setting('TestSet', 't2', True, bool),
+                    't3': Setting('TestSet', 't3', 'abc123', str)
+                    }
+        res = self.settings_saveload(settings)
+
         matches = True
         for setting in settings:
             if setting not in res:
                 matches = False
         self.assertTrue(matches)
-    
+
     def test_settings_values_match(self):
-        settings = { 't1': 1, 't2': True, 't3': 'abc123'}
-        res = self.SettingSaveLoad(settings)
-        
+        settings = {'t1': Setting('TestSet', 't1', 1, int),
+                    't2': Setting('TestSet', 't2', True, bool),
+                    't3': Setting('TestSet', 't3', 'abc123', str)
+                    }
+        res = self.settings_saveload(settings)
+
         matches = True
-        ## TODO: Implement
+        for setting in settings:
+            if setting not in res:
+                matches = False
+                break
+            if settings[setting].valtype(res[setting].value) \
+                    != settings[setting].value:
+                matches = False
+                break
+
         self.assertTrue(matches)
 
 
