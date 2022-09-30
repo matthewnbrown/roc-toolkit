dfd9a28c8b4acb1d1e87f6858a041abd30b07be2
Matt Brown <gitmattb@gmail.com>
2022-09-30 14:27:34 -0700

    Add test for base page class

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -1,2 +1,32 @@
-import os
 import unittest
+from bs4 import BeautifulSoup
+
+from rocalert.pages import RocPage
+
+
+def _get_dir():
+    return __file__[:__file__.rfind('\\')+1]
+
+
+class PageLoggedInTest(unittest.TestCase):
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+
+    def test_logged_in(self):
+        filepath = _get_dir() + '/testpages/loginstatus/true.html'
+        with open(filepath) as f:
+            text = f.read()
+            soup = BeautifulSoup(text, 'html.parser')
+
+        page = RocPage(soup)
+        self.assertTrue(page.logged_in, 'Page is not logged in.')
+
+    def test_notlogged_in(self):
+        filepath = _get_dir() + '/testpages/loginstatus/false.html'
+
+        with open(filepath) as f:
+            text = f.read()
+            soup = BeautifulSoup(text, 'html.parser')
+
+        page = RocPage(soup)
+        self.assertFalse(page.logged_in, 'Page is logged in.')
