423737562024266b29f490a12dd3da1364a03c22
Matt Brown <gitmattb@gmail.com>
2022-10-26 15:33:06 -0700

    Add bftarget class

--- rocalert/rocaccount.py
+++ rocalert/rocaccount.py
@@ -4,6 +4,18 @@ from rocalert.rocpurchases.rocpurchtools \
     ALL_ITEM_DETAILS
 
 
+class BattlefieldTarget:
+    def __init__(self, id: str, rank: int, name: str, alliance: str,
+                 tff: int, tfftype: str, gold: int) -> None:
+        self.id = id
+        self.rank = rank
+        self.name = name
+        self.alliance = alliance
+        self.tff = tff
+        self.tfftype = tfftype
+        self.gold = gold
+
+
 class ROCStats:
     STAT_IDS = {'keyfound', 'lastactive', 'rank', 'highestrank',
                 'armysize', 'tbg', 'sa', 'da', 'spy', 'sentry'}
