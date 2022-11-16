b755ced129179e96edeb9263ea926f35220e5655
Matt Brown <gitmattb@gmail.com>
2022-11-15 20:40:34 -0800

    Add ItemCostPair default vals for models

--- rocalert/rocpurchases/models.py
+++ rocalert/rocpurchases/models.py
@@ -1,68 +1,77 @@
 import dataclasses
-import typing
+
+
+@dataclasses.dataclass(frozen=True)
+class ItemCostPair:
+    count: int = 0
+    cost: int = 0
+
+    @property
+    def total_cost(self) -> int:
+        return self.count * self.cost
 
 
 @dataclasses.dataclass
 class TrainingModel:
-    untrained_soldiers: typing.Tuple(int, int)
-    attack_soldiers: typing.Tuple(int, int)
-    defense_soldiers: typing.Tuple(int, int)
-    spies: typing.Tuple(int, int)
-    sentries: typing.Tuple(int, int)
-    attack_mercs: typing.Tuple(int, int)
-    defense_mercs: typing.Tuple(int, int)
-    untrained_mercs: typing.Tuple(int, int)
+    untrained_soldiers: ItemCostPair = ItemCostPair()
+    attack_soldiers: ItemCostPair = ItemCostPair()
+    defense_soldiers: ItemCostPair = ItemCostPair()
+    spies: ItemCostPair = ItemCostPair()
+    sentries: ItemCostPair = ItemCostPair()
+    attack_mercs: ItemCostPair = ItemCostPair()
+    defense_mercs: ItemCostPair = ItemCostPair()
+    untrained_mercs: ItemCostPair = ItemCostPair()
 
 
 @dataclasses.dataclass
 class ArmoryModel:
-    dagger: typing.Tuple(int, int)
-    maul: typing.Tuple(int, int)
-    blade: typing.Tuple(int, int)
-    excalibur: typing.Tuple(int, int)
-    sai: typing.Tuple(int, int)
-    shield: typing.Tuple(int, int)
-    mithril: typing.Tuple(int, int)
-    dragonskin: typing.Tuple(int, int)
-    cloak: typing.Tuple(int, int)
-    hook: typing.Tuple(int, int)
-    pickaxe: typing.Tuple(int, int)
-    horn: typing.Tuple(int, int)
-    guard_dog: typing.Tuple(int, int)
-    torch: typing.Tuple(int, int)
+    dagger: ItemCostPair = ItemCostPair()
+    maul: ItemCostPair = ItemCostPair()
+    blade: ItemCostPair = ItemCostPair()
+    excalibur: ItemCostPair = ItemCostPair()
+    sai: ItemCostPair = ItemCostPair()
+    shield: ItemCostPair = ItemCostPair()
+    mithril: ItemCostPair = ItemCostPair()
+    dragonskin: ItemCostPair = ItemCostPair()
+    cloak: ItemCostPair = ItemCostPair()
+    hook: ItemCostPair = ItemCostPair()
+    pickaxe: ItemCostPair = ItemCostPair()
+    horn: ItemCostPair = ItemCostPair()
+    guard_dog: ItemCostPair = ItemCostPair()
+    torch: ItemCostPair = ItemCostPair()
 
 
 @dataclasses.dataclass
 class TrainingPurchaseModel:
-    attack_soldiers: int
-    defense_soldiers: int
-    spies: int
-    sentries: int
-    attack_mercs: int
-    defense_mercs: int
-    untrained_mercs: int
-    sell_attack_soldiers: int
-    sell_defense_soldiers: int
-    sell_spies: int
-    sell_sentries: int
-    sell_attack_mercs: int
-    sell_defense_mercs: int
-    sell_untrained_mercs: int
+    attack_soldiers: int = ItemCostPair()
+    defense_soldiers: int = ItemCostPair()
+    spies: int = ItemCostPair()
+    sentries: int = ItemCostPair()
+    attack_mercs: int = ItemCostPair()
+    defense_mercs: int = ItemCostPair()
+    untrained_mercs: int = ItemCostPair()
+    sell_attack_soldiers: int = ItemCostPair()
+    sell_defense_soldiers: int = ItemCostPair()
+    sell_spies: int = ItemCostPair()
+    sell_sentries: int = ItemCostPair()
+    sell_attack_mercs: int = ItemCostPair()
+    sell_defense_mercs: int = ItemCostPair()
+    sell_untrained_mercs: int = ItemCostPair()
 
 
 @dataclasses.dataclass
 class ArmoryPurchaseModel:
-    dagger: int
-    maul: int
-    blade: int
-    excalibur: int
-    sai: int
-    shield: int
-    mithril: int
-    dragonskin: int
-    cloak: int
-    hook: int
-    pickaxe: int
-    horn: int
-    guard_dog: int
-    torch: int
+    dagger: int = ItemCostPair()
+    maul: int = ItemCostPair()
+    blade: int = ItemCostPair()
+    excalibur: int = ItemCostPair()
+    sai: int = ItemCostPair()
+    shield: int = ItemCostPair()
+    mithril: int = ItemCostPair()
+    dragonskin: int = ItemCostPair()
+    cloak: int = ItemCostPair()
+    hook: int = ItemCostPair()
+    pickaxe: int = ItemCostPair()
+    horn: int = ItemCostPair()
+    guard_dog: int = ItemCostPair()
+    torch: int = ItemCostPair()
