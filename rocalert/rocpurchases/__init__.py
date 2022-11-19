0cada7203598108344a78e981d084e04f44af68e
Matt Brown <gitmattb@gmail.com>
2022-11-18 20:38:26 -0800

    Add uses for init

--- rocalert/rocpurchases/__init__.py
+++ rocalert/rocpurchases/__init__.py
@@ -3,3 +3,15 @@ from .rocpurchtools import RocItem, RocItemGroup, ALL_ITEM_DETAILS
 from .roc_trainer import ROCTrainingPurchaseCreatorABC,\
     ROCTrainingPayloadCreatorABC, ROCTrainingPayloadCreator,\
     ROCTrainingDumpPurchaseCreator, ROCTrainingWeaponMatchPurchaseCreator
+
+if __name__ == '__main__':
+    print('don\'t run this file')
+    ROCBuyer()
+    RocItem()
+    ROCTrainingPurchaseCreatorABC()
+    ROCTrainingPayloadCreatorABC()
+    ROCTrainingPayloadCreator()
+    ROCTrainingDumpPurchaseCreator()
+    ROCTrainingWeaponMatchPurchaseCreator()
+    RocItemGroup()
+    ALL_ITEM_DETAILS
