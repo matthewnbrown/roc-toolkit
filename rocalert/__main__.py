4ec04693c107b6f3c00dd123054b2f89dfe3ff88
Matt Brown <gitmattb@gmail.com>
2022-11-20 14:11:45 -0800

    fix: :ambulance: remove a header that caused http request to fail
    encoding header was causing strange responses

--- rocalert/__main__.py
+++ rocalert/__main__.py
@@ -40,7 +40,6 @@ def run():
     default_headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml'
                   + ';q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'en-US,en;q=0.5',
         'Connection': 'keep-alive',
         'Sec-Fetch-Dest': 'document',
