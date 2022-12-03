e05d3ed6a2151932c208c3efc2f965cd11426c55
Matt Brown <gitmattb@gmail.com>
2022-12-03 10:12:28 -0800

    implement settings base url generator

--- rocalert/services/urlgenerator.py
+++ rocalert/services/urlgenerator.py
@@ -1,16 +1,39 @@
 import abc
 
+import rocalert.roc_settings as rocsettings
+
 
 class URLNotFoundError(Exception):
     pass
 
 
-class ROCUrlGeneratorABC(abc.ABC):
-    def get_page_url(page: str) -> str:
+class ROCUrlGenerator(abc.ABC):
+    @abc.abstractmethod
+    def get_page_url(self, page: str) -> str:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def get_home() -> str:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def get_armory() -> str:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def get_training() -> str:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def get_base() -> str:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def get_recruit() -> str:
         raise NotImplementedError()
 
 
-class ROCDecryptUrlGenerator(ROCUrlGeneratorABC):
+class ROCDecryptUrlGenerator(ROCUrlGenerator):
     def __init__(self) -> None:
         pass
 
@@ -18,9 +41,25 @@ class ROCDecryptUrlGenerator(ROCUrlGeneratorABC):
         pass
 
 
-class ROCSiteSettingsUrlGenerator(ROCUrlGeneratorABC):
-    def __init__(self, sitesettings) -> None:
+class ROCSiteSettingsUrlGenerator(ROCUrlGenerator):
+    def __init__(self, sitesettings: rocsettings.SiteSettings) -> None:
         self._sitesettings = sitesettings
 
-    def get_page_url(page: str) -> str:
-        raise NotImplementedError()
+    def get_page_url(self, page: str) -> str:
+        if page not in self._sitesettings.get_settings():
+            raise URLNotFoundError(f'{page} url not known')
+
+    def get_home(self) -> str:
+        return self._sitesettings.get_home()
+
+    def get_armory(self) -> str:
+        return self._sitesettings.get_armory()
+
+    def get_training(self) -> str:
+        return self._sitesettings.get_training()
+
+    def get_base(self) -> str:
+        return self._sitesettings.get_home() + '/base.php'
+
+    def get_recruit(self) -> str:
+        return self._sitesettings.get_recruit()
