e31c74a769a3712c0aa91b611c8ffb4bcad748b8
Matt Brown <gitmattb@gmail.com>
2022-12-20 19:02:05 -0800

    feat: add max time to sleep past nightmode
    properly return nightmode waittime value

--- rocalert/services/sleeptimer.py
+++ rocalert/services/sleeptimer.py
@@ -20,7 +20,8 @@ class SleepTimer(SleepTimerABC):
             self,
             user_settings: UserSettings,
             randomlowhigh: typing.Callable[[float, float], float] = None,
-            current_time_getter: typing.Callable[[], datetime.datetime] = None
+            current_time_getter: typing.Callable[[], datetime.datetime] = None,
+            max_postnightmode_sleeptime_mins: int = None
             ) -> None:
         self._usersettings = user_settings
 
@@ -28,9 +29,13 @@ class SleepTimer(SleepTimerABC):
             randomlowhigh = random.uniform
         if current_time_getter is None:
             current_time_getter = datetime.datetime.now
+        if max_postnightmode_sleeptime_mins is None:
+            min_regtime, _ = user_settings.regular_waittimes_seconds
+            max_postnightmode_sleeptime_mins = min_regtime // 60
 
         self._randomfunc = randomlowhigh
         self._current_time = current_time_getter
+        self._max_postnightmode_naptime_mins = max_postnightmode_sleeptime_mins
 
     def time_is_in_nightmode(self, time: datetime.datetime) -> bool:
         if not self._usersettings.use_nightmode:
@@ -73,7 +78,10 @@ class SleepTimer(SleepTimerABC):
                 end_date = datetime.datetime.combine(
                     (now + datetime.timedelta(days=1)).date, nightmode_end)
 
-            waittime = (end_date - now).total_seconds + self._randomfunc(5)*60
+            waittime = (end_date - now).total_seconds + \
+                self._randomfunc(0, self._max_postnightmode_naptime_mins)*60
+
+        return waittime.total_seconds()
 
     def _calculate_regular_sleeptime(self) -> float:
         mintime, maxtime = self._usersettings.regular_waittimes_seconds
