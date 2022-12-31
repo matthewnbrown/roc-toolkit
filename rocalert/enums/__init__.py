05086b044a2df949872fa53af3e50efa03032933
Matt Brown <gitmattb@gmail.com>
2022-12-30 18:17:00 -0800

    refactor: move rocpagetype to enums

--- rocalert/enums/__init__.py
+++ rocalert/enums/__init__.py
@@ -1,2 +1,3 @@
 from .weapons import Weapon  # noqa F401
 from .weapontypes import WeaponType  # noqa F401
+from .rocpagetype import RocPageType  # noqa F401
--- rocalert/pages/genericpages.py
+++ rocalert/pages/genericpages.py
@@ -1,7 +1,8 @@
-import enum
 from dataclasses import dataclass
 from typing import Optional
 from datetime import datetime
+
+import rocalert.enums as rocenums
 
 
 def rocnum_to_int(num_as_str: str):
@@ -14,40 +15,10 @@ def int_to_rocnum(num: int):
     return f'{num:,}'
 
 
-class RocPageType(enum.Enum):
-    UNKNOWN = 1
-    HOME = 2
-    BASE = 3
-    TRAINING = 4
-    ARMORY = 5
-    RECRUIT = 6
-    KEEP = 7
-    BATTLEFIELD = 8  # Not Implemented
-    BUILDINGS_AND_SKILLS = 9  # Not Implemented
-    MAIL = 10  # Not Implemented
-    BATTLE_LOG = 11  # Not Implemented
-    ACTIVITY_LOG = 12  # Not Implemented
-    TARGET_STATS = 13  # Not Implemented
-    TARGET_ATTACK = 14  # Not Implemented
-    TARGET_PROBE = 15  # Not Implemented
-    TARGET_SPY = 16  # Not Implemented
-    TARGET_SENTRY = 17  # Not Implemented
-    TARGET_SPITE = 18  # Not Implemented
-    INTEL_FILE = 19  # Overall recent intel  # Not Implemented
-    INTEL_DETAIL = 20  # Specific spy report  # Not Implemented
-    ALL_SAB_LOG = 21  # Not Implemented
-    ERROR = 22
-    ATTACK_LOG = 23  # Not Implemented
-    ATTACK_DETAIL = 24  # Not Implemented
-    ALLIANCE = 25  # Not Implemented
-    SEND_CREDITS = 26  # Not Implemented
-    SEND_MESSAGE = 27  # Not Implemented
-    SEND_DOGE = 28  # Not Implemented
-
-
 @dataclass(frozen=True)
 class RocPage:
-    page_type: RocPageType
+    page_type: rocenums.RocPageType
+    creation_date: datetime
     logged_in: bool
 
 
